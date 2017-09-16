38abd52312c34f4b254e6d8945b7836f35a2f46e
jspiner <jspiner@jspinerui-MacBook-Pro.local>
2017-09-16 17:34:36 +0900

    add text loading

--- app/src/main/java/com/noname/tenminute/Fragment/BaseFragment.java
+++ app/src/main/java/com/noname/tenminute/Fragment/BaseFragment.java
@@ -1,10 +1,15 @@
 package com.noname.tenminute.Fragment;
 
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 /**
  * Created by PJC on 2017-07-30.
  */
 
 public class BaseFragment extends Fragment {
+
+    protected void showToast(String text){
+        Toast.makeText(getContext(), text, Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/noname/tenminute/Fragment/HomeFragment.java
+++ app/src/main/java/com/noname/tenminute/Fragment/HomeFragment.java
@@ -5,17 +5,38 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.noname.tenminute.R;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by PJC on 2017-07-30.
  */
 
 public class HomeFragment extends BaseFragment {
+
+    @BindView(R.id.tv_home_waiting)
+    protected TextView tvWaiting;
+
+    private PublishSubject<Integer> subject = PublishSubject.create();
+    private Disposable loadinDisposable = null;
+
+    private boolean isCalling = false;
 
     @Nullable
     @Override
@@ -27,11 +48,48 @@ public class HomeFragment extends BaseFragment {
 
     @OnClick(R.id.imv_call)
     public void onCallButton() {
+        if (isCalling) {
+            stopLoadingText();
+        } else {
+            startLoadingText();
+        }
 
+        isCalling = !isCalling;
     }
 
     @OnClick(R.id.btn_set_if)
     public void onConditionButton() {
 
+    }
+
+    private void startLoadingText() {
+        tvWaiting.setVisibility(View.VISIBLE);
+        loadinDisposable = subject.delay(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(count -> {
+                    tvWaiting.setText(
+                            "통화 연결중" + repeat(".", count)
+                    );
+
+                    subject.onNext(
+                            (++count) % 5
+                    );
+                });
+        subject.onNext(0);
+    }
+
+    private void stopLoadingText() {
+        showToast("통화연결이 취소되었습니다.");
+
+        tvWaiting.setVisibility(View.INVISIBLE);
+        loadinDisposable.dispose();
+    }
+
+    private String repeat(String text, int count) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            builder.append(text);
+        }
+        return builder.toString();
     }
 }
